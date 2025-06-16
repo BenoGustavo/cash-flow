3df19b08517d2159dcbd2ae7b54b195dee98f76d
Gustavo Gorges <gustavo.gorges@aqtech.com>
2025-06-16 15:44:28 -0300

    Add unit tests for RegisterExpenseValidator
    Created unit tests for the RegisterExpenseValidator class, covering scenarios such as successful validation, empty titles, future dates, invalid payment methods, and invalid amounts using FluentAssertions for better readability.
    
    Updated Validators.Tests.csproj to target .NET 8.0, enable implicit usings and nullable reference types, and added necessary NuGet package references and project dependencies for testing.

---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,100 @@
+﻿using Application.UseCases.Expenses.Register;
+using CommonTestUtilities.Requests;
+using Communication.Enums;
+using Exception;
+using FluentAssertions;
+
+namespace Validators.Tests.Expenses.Register
+{
+    public class RegisterExpenseValidatorTests
+    {
+        [Fact]
+        public void Success()
+        {
+            // Arrange
+            var validator = new RegisterExpenseValidator();
+            var request = RequestRegisterExpenseJsonBuilder.Build();
+
+            // Act
+            var result = validator.Validate(request);
+
+            // Assert
+            result.IsValid.Should().BeTrue();
+
+            // Native way
+            //Assert.True(result.IsValid, "Validation should succeed for valid request.");
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("        ")]
+        [InlineData(null)]
+        public void Error_EmptyTitle(string title)
+        {
+            // Arrange
+            var validator = new RegisterExpenseValidator();
+            var request = RequestRegisterExpenseJsonBuilder.Build();
+            request.Title = title;
+
+            // Act
+            var result = validator.Validate(request);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.TITLE_REQUIRED));
+        }
+
+        [Fact]
+        public void Error_Date_Future()
+        {
+            // Arrange
+            var validator = new RegisterExpenseValidator();
+            var request = RequestRegisterExpenseJsonBuilder.Build();
+            request.Date = DateTime.UtcNow.AddDays(1);
+
+            // Act
+            var result = validator.Validate(request);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.EXPENSES_CANNOT_FOR_THE_FUTURE));
+        }
+
+        [Fact]
+        public void Error_Invalid_PaymentMethod()
+        {
+            // Arrange
+            var validator = new RegisterExpenseValidator();
+            var request = RequestRegisterExpenseJsonBuilder.Build();
+                
+            request.PaymentMethod = (PaymentMethodEnum)999;
+
+            // Act
+            var result = validator.Validate(request);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.INVALID_PAYMENT_METHOD));
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void Error_Invalid_Amount(decimal amount)
+        {
+            // Arrange
+            var validator = new RegisterExpenseValidator();
+            var request = RequestRegisterExpenseJsonBuilder.Build();
+
+            request.Amount = amount;
+
+            // Act
+            var result = validator.Validate(request);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO));
+        }
+
+    }
+}
