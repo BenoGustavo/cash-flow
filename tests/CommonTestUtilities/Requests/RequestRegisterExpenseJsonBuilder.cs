794e2fbb50f95bd2f2719e41f501edfc45fef8ad
Gustavo Gorges <gustavo.gorges@aqtech.com>
2025-06-16 15:44:59 -0300

    Add Validators.Tests and CommonTestUtilities projects
    Updated the solution file `CashFlow.sln` to include two new projects: `Validators.Tests` and `CommonTestUtilities`, with corresponding build configurations.
    
    Created `CommonTestUtilities.csproj` targeting .NET 8.0, utilizing the `Bogus` package for generating fake data and referencing the `Communication` project.
    
    Introduced `RequestRegisterExpenseJsonBuilder` class to generate fake data for `RequestRegisterExpenseJson` objects.
    
    Added `Validator.Tests.vbproj`, targeting .NET 8.0, with references to testing packages like `coverlet.collector` and `xunit`, and organized folder structure for expense registration tests.

---
+++ tests/CommonTestUtilities/Requests/RequestRegisterExpenseJsonBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Bogus;
+using Communication.Requests;
+using Communication.Enums;
+
+namespace CommonTestUtilities.Requests;
+public class RequestRegisterExpenseJsonBuilder
+{
+    public static RequestRegisterExpenseJson Build()
+    {
+        var faker =  new Faker("en");
+
+        return new RequestRegisterExpenseJson
+        {
+            Title = faker.Commerce.ProductName(),
+            Date = faker.Date.Past(),
+            Description = faker.Lorem.Sentence(10),
+            PaymentMethod = faker.PickRandom<PaymentMethodEnum>(),
+            Amount = faker.Random.Decimal(min:1, max:1000),
+        };
+    }
+}
