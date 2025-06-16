959e2983fd72f6bb53f91e3be55d1a5d1e374a49
Gustavo Gorges <gustavo.gorges@aqtech.com>
2025-06-16 13:46:19 -0300

    create culture middleware

---
+++ src/CashFlow.Api/Middleware/CultureMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace CashFlow.Api.Middleware
+{
+    public class CultureMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public CultureMiddleware(RequestDelegate next)
+        {
+            this._next = next ?? throw new ArgumentNullException(nameof(next));
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            string? culture = context.Request.Headers.AcceptLanguage.FirstOrDefault(); 
+
+            var cultureInfo = new CultureInfo(culture ?? "en");
+
+            if (string.IsNullOrWhiteSpace(culture) == false) { 
+                cultureInfo = new CultureInfo(culture);
+            }
+
+            CultureInfo.CurrentCulture = cultureInfo;
+            CultureInfo.CurrentUICulture = cultureInfo;
+
+            await _next(context);
+        }
+    }
+}
