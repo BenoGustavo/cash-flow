1310a3f666942716465c3b26dd28eea2c2b2c41c
Gustavo Gorges <gustavo.gorges@aqtech.com>
2025-06-25 16:40:04 -0300

    Add response type annotations to Register method in ExpensesController

--- src/CashFlow.Api/Controller/ExpensesController.cs
+++ src/CashFlow.Api/Controller/ExpensesController.cs
@@ -1,5 +1,6 @@
 ﻿using Application.UseCases.Expenses.Register;
 using Communication.Requests;
+using Communication.Responses;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CashFlow.Api.Controller
@@ -9,6 +10,8 @@ namespace CashFlow.Api.Controller
     public class ExpensesController : ControllerBase
     {
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(ResponseRegisteredExpenseJson))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ResponseErrorJson))]
         public async Task<IActionResult> Register([FromBody] RequestRegisterExpenseJson request, [FromServices] IRegisterExpensesUseCase useCase)
         {
             var response = await useCase.Execute(request);
