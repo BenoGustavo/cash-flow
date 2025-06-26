a2f350634410dc59dce562864fb0b74a5b91e571
Gustavo Gorges <gustavo.gorges@aqtech.com>
2025-06-26 10:29:13 -0300

    Adds an endpoint to retrieve all expenses
    Implements a new GET endpoint in the expenses controller to fetch all recorded expenses.
    
    The endpoint returns a list of expenses with a 200 OK status, or a 204 No Content response if no expenses exist.

--- src/CashFlow.Api/Controller/ExpensesController.cs
+++ src/CashFlow.Api/Controller/ExpensesController.cs
@@ -1,4 +1,5 @@
-﻿using Application.UseCases.Expenses.Register;
+﻿using Application.UseCases.Expenses.GetAll;
+using Application.UseCases.Expenses.Register;
 using Communication.Requests;
 using Communication.Responses;
 using Microsoft.AspNetCore.Mvc;
@@ -21,6 +22,18 @@ namespace CashFlow.Api.Controller
                 new { id = response.Title },
                 response
             );
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseExpensesJson))]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> GetAll([FromServices] IGetAllExpensesUseCase useCase)
+        {
+            var response = await useCase.Execute();
+
+            if (response.Expenses.Count != 0) return Ok(response);
+
+            return NoContent();
         }
     }
 }
